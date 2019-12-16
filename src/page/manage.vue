1e6dd52c428e8326576e5b82d5036c3a83dee6df
杨臻 <yangzhen@lovedabai.com>
2019-12-16 19:47:43 +0800

    coco ssd

--- src/page/manage.vue
+++ src/page/manage.vue
@@ -109,7 +109,7 @@
     } from '../util/posenet_util';
 
     const modelParams = {
-        flipHorizontal: false,   // flip e.g for video
+        flipHorizontal: true,   // flip e.g for video
         maxNumBoxes: 20,        // maximum number of boxes to detect
         iouThreshold: 0.5,      // ioU threshold for non-max suppression
         scoreThreshold: 0.6,    // confidence threshold for predictions.
